818f703f21abccaffff87e419b77d8aef22b6a31
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2022-09-15 12:34:34 +0530

    my changes

---
+++ core_php/Function_simple/function.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+$a=20;
+$b=10;
+echo $sum=$a+$b;
+*/
+
+
+// user defined function
+
+/*
+function ami()
+{
+	$a=10;
+	$b=20;
+	echo $sum=$a+$b."<br>";
+}
+ami();
+*/
+
+
+function priya()
+{
+	$a=30;
+	$b=20;
+	echo $sum=$a+$b."<br>";
+}
+priya();
+
+/*
+//buil in function predifined function
+$abc="Raj nagar";
+
+echo strlen($abc);
+
+*/
+
+// Return in function
+
+/*
+function raj()
+{
+	$a=10;
+	$b=20;
+	return $sum=$a+$b."<br>";
+}
+echo raj();
+*/
+
+
+
+function sum($a,$b)
+{
+	echo $sum=$a+$b."<br>";
+}
+sum(5,7);
+sum(7,7);
+sum(5,9);
+sum(10,7);
+
+
+/*
+function select($tbl)
+{
+	echo $sel="select * from $tbl<br>";
+}
+select('feedback');
+select('customer');
+select('contact');
+select('blog');
+*/
+?>
